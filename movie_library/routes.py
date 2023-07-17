cfb3111e330370e633e2b65ebc7434b5f2528f91
Asher Etherington <asherce21@gmail.com>
2023-07-16 23:04:19 -0700

    working

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,58 +1,57 @@
-import uuid
-from movie_library.models import Movie, User
-from passlib.hash import pbkdf2_sha256
-from dataclasses import asdict
-import datetime
 import functools
-from flask import (Blueprint, 
-                   render_template, 
-                   session, 
-                   redirect, 
-                   request, 
-                   current_app, 
-                   url_for,
-                   abort,
-                   flash
-                   )
-from movie_library.forms import (MovieForm, 
-                                ExtendedMovieForm,
-                                RegisterForm, 
-                                LoginForm,
-                                )
+import uuid
+import datetime
+from dataclasses import asdict
+
+from flask import (
+    Blueprint,
+    current_app,
+    flash,
+    redirect,
+    render_template,
+    session,
+    url_for,
+    request,
+)
+from movie_library.forms import LoginForm, RegisterForm, MovieForm, ExtendedMovieForm
+from movie_library.models import User, Movie
+from passlib.hash import pbkdf2_sha256
+
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
 )
+
 
 def login_required(route):
     @functools.wraps(route)
     def route_wrapper(*args, **kwargs):
         if session.get("email") is None:
             return redirect(url_for(".login"))
-        
-        return route(*args, **kwargs)
-    return route_wrapper
 
+        return route(*args, **kwargs)
+
+    return route_wrapper
 
 #base route
 @pages.route("/")
 @login_required#user must be logged in to see this page
 def index():
-    #get current user data
+    #get current user data and make a User object with it
     user_data = current_app.db.user.find_one({"email": session["email"]})
     user = User(**user_data)
-    #get movie data that is in the current users list of movies
+    #get movie data that is in the current users list of movies and make a list of Movie objects
     movie_data = current_app.db.movie.find({"_id": {"$in": user.movies}})
     movies = [Movie(**movie) for movie in movie_data]
+
     return render_template(
         "index.html",
         title="Movies Watchlist",
-        movies_data=movies
+        movies_data=movies,
     )
 
-
 #route for registering users
-@pages.route("/add", methods=["GET", "POST"])
+@pages.route("/register", methods=["POST", "GET"])
 def register():
     #if session already has a logged in email redirect to base route
     if session.get("email"):
@@ -64,7 +63,7 @@ def register():
         user = User(
             _id=uuid.uuid4().hex,
             email=form.email.data,
-            password=pbkdf2_sha256.hash(form.password.data)
+            password=pbkdf2_sha256.hash(form.password.data),
         )
         #add user to the database of users
         current_app.db.user.insert_one(asdict(user))
@@ -73,8 +72,9 @@ def register():
         #redirect user to login page
         return redirect(url_for(".login"))
 
-    return render_template("register.html", title="Movies Watchlist - Register", form=form)
-
+    return render_template(
+        "register.html", title="Movies Watchlist - Register", form=form
+    )
 
 #route for logging in
 @pages.route("/login", methods=["GET", "POST"])
@@ -84,17 +84,16 @@ def login():
         return redirect(url_for(".index"))
     #create form and check validation
     form = LoginForm()
+
     if form.validate_on_submit():
         #try to find the user in the db using the email from the form
         user_data = current_app.db.user.find_one({"email": form.email.data})
         #if couldnt find user data using the email flash message
         if not user_data:
-            flash("Login info not correct", category="danger")
+            flash("Login credentials not correct", category="danger")
             return redirect(url_for(".login"))
-        
         #create a user object using user_data that we got using the email
         user = User(**user_data)
-
         # check if the form password equals the user password
         if user and pbkdf2_sha256.verify(form.password.data, user.password):
             #populate the session with anything that we need and redirect to base
@@ -102,58 +101,46 @@ def login():
             session["email"] = user.email
 
             return redirect(url_for(".index"))
-        
-        flash("Login info not correct", category="danger")
-    
-    #if user couldnt be verified return to login page
-    return render_template("login.html", title="Movie Watchlist - Login", form=form)
 
+        flash("Login credentials not correct", category="danger")
+    #if user couldnt be verified return to login page
+    return render_template("login.html", title="Movies Watchlist - Login", form=form)
 
 #logout route
 @pages.route("/logout")
 def logout():
     #clear everything from session except the theme
-    current_theme = session["theme"]
-    session.clear()
-
-    session["theme"] = current_theme
+    del session["email"]
+    del session["user_id"]
 
     return redirect(url_for(".login"))
 
-
 #route for adding movies using the form
 @pages.route("/add", methods=["GET", "POST"])
-@login_required#user must be logged in
+@login_required
 def add_movie():
-    #create a form that is a MovieForm class
     form = MovieForm()
 
-    #checks if the form has been run and checks validation
-    # if validation fails the errors for the fields get passed
     if form.validate_on_submit():
-        #create a movie class from models.py and populate it with the form values
         movie = Movie(
-            _id= uuid.uuid4().hex,
-            title= form.title.data,
-            director= form.director.data,
-            year= form.year.data
-            )
-        #add the movie to mongodb as a dictionary
-        # (all the default values will be included for the other fields)
+            _id=uuid.uuid4().hex,
+            title=form.title.data,
+            director=form.director.data,
+            year=form.year.data,
+        )
+
         current_app.db.movie.insert_one(asdict(movie))
-        #add the movie to the users list of movies
+
         current_app.db.user.update_one(
             {"_id": session["user_id"]}, {"$push": {"movies": movie._id}}
         )
 
-        return redirect(url_for(".index"))
-
+        return redirect(url_for(".movie", _id=movie._id))
 
     return render_template(
-        "new_movie.html", 
-        title="Movies Watchlist - Add Movie", 
-        form=form
-        )
+        "new_movie.html", title="Movies Watchlist - Add Movie", form=form
+    )
+
 
 
 
